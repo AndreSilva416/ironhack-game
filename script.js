9c37c39bbf52b11d018888ca89d58429752de1e5
Andre Silva <and.silva416@gmail.com>
2021-01-27 13:09:53 +0000

    updated

--- script.js
+++ script.js
@@ -3,77 +3,103 @@ ctx = canvas.getContext("2d");
 
 canvas.style.border = '2px solid black'
 let intervalID = 0
+let score = 0;
 
+// Splash = document.querySelector(."splash")
+// Splash.style.display = ""
 
-// Images
-// let backImg1 = document.createElement('img')
-// backImg1.src = 'images/sky.jpg'
+// Background
+let backImg1 = document.createElement('img')
+backImg1.src = 'images/sky.jpg'
 // let backImg2 = document.createElement('img')
 // backImg2.src = ''
 
-let ship = document.createElement('img')
-ship.src = 'images/Ship.png' 
+//Meteors
+let player = document.createElement('img')
+player.src = 'images/player.png' 
+let meteor1 = document.createElement('img')
+meteor1.src = 'images/meteor1.png'
+let meteor2 = document.createElement('img')
+meteor2.src = 'images/meteor2.png'
+let meteor3 = document.createElement('img')
+meteor3.src = 'images/meteor3.png'
+let meteor4 = document.createElement('img')
+meteor4.src = 'images/meteor4.png'
+let meteors = [{x:0 , y:canvas.width - 900, img: meteor1}]
+let meteorImgs = [meteor1, meteor2, meteor3, meteor4]
 
-// let meteor1 = document.createElement('img')
-// meteor1.src = 'images/meteor1'
-// let meteor2 = document.createElement('img')
-// meteor2.src = 'images/meteor2'
-// let meteor3 = document.createElement('img')
-// meteor3.src = 'images/meteor3'
-// let meteor4 = document.createElement('img')
-// meteor4.src = 'images/meteor4'
-
-// Controler
-// let isLeftArrow = false;
-// let isRightArrow = false
-    
-// document.addEventListener('keydown', (event) => {
-//     if (event.key == "ArrowRight") {
-//        isRightArrow = true;
-//        isLeftArrow = false;
-//     }
-//     else if (event.key == "ArrowLeft") {
-//        isRightArrow = false;
-//        isLeftArrow = true;
-//     }
-// })
-
-// document.addEventListener('keyup', (event) => {
-//     isRightArrow = false;
-//     isLeftArrow = false;
-// })
+// Player
+let isLeftArrow = false;
+let isRightArrow = false
+let incrementPlayer = 5;
+let playerX = 250
+let playerWidth = 100
 
 
-function draw() {
-
-    function background(){
-        ctx.drawImage(backImg1, 0, 0)
+document.addEventListener('keydown', (event) => {
+    if (event.key == "ArrowRight") {
+       isRightArrow = true;
+       isLeftArrow = false;
     }
+    else if (event.key == "ArrowLeft") {
+       isRightArrow = false;
+       isLeftArrow = true;
+    }
+})
+
+document.addEventListener('keyup', (event) => {
+    isRightArrow = false;
+    isLeftArrow = false;
+})
 
 
-   function ship() {
-       ctx.drawImage(ship, 0, 0)
-  }
-
-//    function Meteors() {
-
-//   }
-
-//    function Collision() {
-
-//   }
-
+function background(){
+        ctx.drawImage(backImg1, 0, 0)
 }
 
-ctx.drawImage(backImg1)
-ctx.drawImage(ship)
+function Player() {   
+    ctx.drawImage(player, playerX, 750)
+  }
 
 
+function Meteors() {
+    
+for(let i=0; i<meteors.length  ; i++) {
+    let meteorImg = meteorImgs[Math.floor(Math.random()* meteorImgs.length)] 
+    ctx.drawImage(meteors[i].img, meteors[i].x, meteors[i].y)
+    meteors[i].y++
 
+    if (meteors[i].y == 50) {
+        meteors.push({
+            x: Math.floor(Math.random()* canvas.width - 50),
+            y: canvas.width - 900,
+            img: meteorImg
+        })
+    }
+    
+    
+ }
+}
 
+//function Collision() {
 
+//}
 
+function draw() {
+  background()
+  Player()
+  Meteors()  
 
+  if (isRightArrow && (playerX + playerWidth < canvas.width)){
+      playerX += incrementPlayer
+  }
+  else if (isLeftArrow && playerX > 0) {
+      playerX -= incrementPlayer
+  }
+
+  
+
+}
 
 
 
@@ -83,5 +109,5 @@ ctx.drawImage(ship)
 window.addEventListener('load', () => {
     intervalID = setInterval(() => {
       requestAnimationFrame(draw)  
-     }, 1000)
+     }, 10)
 })
